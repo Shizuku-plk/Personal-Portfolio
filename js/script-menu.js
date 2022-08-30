644bec45a2c990d0a33583478e6cbc42fcef5a9f
Esteban Esquivel <esquivelesteban0@gmail.com>
2022-08-30 11:49:20 -0500

    add a hamburger menu using javascript event listeners

---
+++ js/script-menu.js
@@ -0,0 +1,6 @@
+const hamburgerButton = document.getElementsByClassName("hamburger-btn")[0]
+const navBar = document.getElementsByClassName("nav-bar")[0]
+
+hamburgerButton.addEventListener('click', () => {
+    navBar.classList.toggle('active')
+})
