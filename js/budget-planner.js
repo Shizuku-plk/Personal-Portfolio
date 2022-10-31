653db6872efd77e904ed85b83e2718080aadfa4b
Esteban Esquivel <99621872+Shizuku-plk@users.noreply.github.com>
2022-10-31 16:51:32 -0500

    add "save" buttons to the income, expenses, and wants sections
    change the onchange events to onclick to trigger the functions when the "save" buttons are clicked. add the new wants section.

--- js/budget-planner.js
+++ js/budget-planner.js
@@ -1,3 +1,4 @@
+"use strict";
 window.addEventListener("load", function() {
     let budgetForm = document.forms["budgetForm"];
     let wages = budgetForm.elements["wages"];
@@ -5,64 +6,101 @@ window.addEventListener("load", function() {
     // highlight the wages field on load
     wages.focus();
     budgetForm.reset();
-    // calculate the income total
-    budgetForm.elements.onchange = calcIncome;
-    // calculate the NEEDS expenses
-    budgetForm.elements.onchange = expensesNeeds;
-
-    // add an event listener for every form element
-    for (let i = 0; i < budgetForm.elements.length; i++) {
-        budgetForm.elements[i].addEventListener("change", calcIncome);
-        budgetForm.elements[i].addEventListener("change", expensesNeeds);
-    }
-
-    function calcIncome() {
-        let wagesValue, otherValue, studentLoansValue, allowanceValue, incomeTotal;
-        let incomeMessage = document.getElementById("income-message");
-
-        wagesValue = parseFloat(wages.value);
-        otherValue = parseFloat(other.value);
-        studentLoansValue = parseFloat(studentLoans.value);
-        allowanceValue = parseFloat(allowance.value);
-
-        incomeTotal = wagesValue + otherValue + studentLoansValue + allowanceValue;
-
-        incomeMessage.textContent = "Total:  " + incomeTotal;
-
-        ValidateIncome();
-    }
-
-    function expensesNeeds() {
-        let rentValue, carPaymentValue, autoInsuranceValue, carMaintenanceValue, gasolineValue,
-        groceriesValue, transportationValue, phoneBillValue, internetBillValue, studentLoanValue,
-        tuitionValue, booksValue, roomValue, otherValue, expensesTotal;
-
-        let expensesMessage = document.getElementById("expenses-message");
-
-        rentValue = parseFloat(rent.value);
-        carPaymentValue = parseFloat(carPayment.value);
-        autoInsuranceValue = parseFloat(autoInsurance.value);
-        carMaintenanceValue = parseFloat(carMaintenance.value);
-        gasolineValue = parseFloat(gasoline.value);
-        groceriesValue = parseFloat(groceries.value);
-        transportationValue = parseFloat(transportation.value);
-        phoneBillValue = parseFloat(phoneBill.value);
-        internetBillValue = parseFloat(internetBill.value);
-        studentLoanValue = parseFloat(studentLoan.value);
-        tuitionValue = parseFloat(tuition.value);
-        booksValue = parseFloat(books.value);
-        roomValue = parseFloat(room.value);
-        otherValue = parseFloat(other.value);
-
-        expensesTotal = rentValue + carPaymentValue + autoInsuranceValue + carMaintenanceValue + gasolineValue + 
-        groceriesValue + transportationValue + phoneBillValue + internetBillValue + studentLoanValue + tuitionValue +
-        booksValue + roomValue + otherValue;
-
-        expensesMessage.textContent = "Total: " + expensesTotal;
-
-    }
 });
+let incomeButton = document.getElementById("incomeButton");
+let expensesButton = document.getElementById("expensesButton");
+let wantsButton = document.getElementById("wantsButton");
 
-function ValidateIncome() {
-    let errorMessage = document.getElementById("error-message");
+// call the calcIncome function 
+incomeButton.onclick = calcIncome;
+// call the expensesNeeds function
+expensesButton.onclick = expensesNeeds;
+// call the wants function
+wantsButton.onclick = wants;
+
+// function to calculate the income
+function calcIncome() {
+    let wagesValue, otherValue, studentLoansValue, allowanceValue, incomeTotal;
+    let incomeMessage = document.getElementById("income-message");
+
+    wagesValue = parseFloat(wages.value);
+    otherValue = parseFloat(other.value);
+    studentLoansValue = parseFloat(studentLoans.value);
+    allowanceValue = parseFloat(allowance.value);
+
+    incomeTotal = wagesValue + otherValue + studentLoansValue + allowanceValue;
+
+    if(isNaN(wagesValue) || isNaN(otherValue) || isNaN(studentLoansValue) || isNaN(allowanceValue)) {
+        incomeMessage.textContent = "Enter a valid input";
+    } else {
+        incomeMessage.textContent = "Total: " + incomeTotal;
+    }
+}
+
+// function to calculate the expenses
+function expensesNeeds() {
+    let rentValue, carPaymentValue, autoInsuranceValue, carMaintenanceValue, gasolineValue,
+    groceriesValue, transportationValue, phoneBillValue, internetBillValue, studentLoanValue,
+    tuitionValue, booksValue, roomValue, otherValue, expensesTotal;
+
+    let expensesMessage = document.getElementById("expenses-message");
+
+    rentValue = parseFloat(rent.value);
+    carPaymentValue = parseFloat(carPayment.value);
+    autoInsuranceValue = parseFloat(autoInsurance.value);
+    carMaintenanceValue = parseFloat(carMaintenance.value);
+    gasolineValue = parseFloat(gasoline.value);
+    groceriesValue = parseFloat(groceries.value);
+    transportationValue = parseFloat(transportation.value);
+    phoneBillValue = parseFloat(phoneBill.value);
+    internetBillValue = parseFloat(internetBill.value);
+    studentLoanValue = parseFloat(studentLoan.value);
+    tuitionValue = parseFloat(tuition.value);
+    booksValue = parseFloat(books.value);
+    roomValue = parseFloat(room.value);
+    otherValue = parseFloat(other.value);
+
+    expensesTotal = rentValue + carPaymentValue + autoInsuranceValue + carMaintenanceValue + gasolineValue + 
+    groceriesValue + transportationValue + phoneBillValue + internetBillValue + studentLoanValue + tuitionValue +
+    booksValue + roomValue + otherValue;
+
+    if (isNaN(rentValue) || isNaN(carPaymentValue) || isNaN(autoInsuranceValue) || isNaN(carMaintenanceValue)
+    || isNaN(gasolineValue) || isNaN(groceriesValue) || isNaN(transportationValue) || isNaN(phoneBillValue)
+    || isNaN(internetBillValue) || isNaN(studentLoanValue) || isNaN(tuitionValue) || isNaN(booksValue)
+    || isNaN(roomValue) || isNaN(otherValue)) {
+        expensesMessage.textContent = "Enter a valid input";
+    } else {
+        expensesMessage.textContent = "Total: " + expensesTotal;
+    }
+}
+
+// function to calculate the wants
+function wants() {
+    let clothingValue, diningValue, mealsValue, alcoholValue, movieValue, gymValue, travelValue, streamingvalue,
+    homeValue, sportsValue, otherValue, wantsTotal;
+
+    let wantsMessage = document.getElementById("wants-message");
+    
+    clothingValue = parseFloat(clothing.value);
+    diningValue = parseFloat(dining.value);
+    mealsValue = parseFloat(meals.value);
+    alcoholValue = parseFloat(alcohol.value);
+    movieValue = parseFloat(movie.value);
+    gymValue = parseFloat(gym.value);
+    travelValue = parseFloat(travel.value);
+    streamingvalue = parseFloat(streaming.value);
+    homeValue = parseFloat(home.value);
+    sportsValue = parseFloat(sports.value);
+    otherValue = parseFloat(other.value);
+
+    wantsTotal = clothingValue + diningValue + mealsValue + alcoholValue + movieValue + 
+    gymValue + travelValue + streamingvalue + homeValue + sportsValue + otherValue;
+
+    if (isNaN(clothingValue) || isNaN(diningValue) || isNaN(mealsValue) || isNaN(alcoholValue)
+    || isNaN(movieValue) || isNaN(gymValue) || isNaN(travelValue) || isNaN(streamingvalue)
+    || isNaN(homeValue) || isNaN(sportsValue) || isNaN(homeValue)) {
+        wantsMessage.textContent = "Enter a valid input";
+    } else {
+        wantsMessage.textContent = "Total: " + wantsTotal;
+    }
 }
