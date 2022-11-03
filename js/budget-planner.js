dad72371824e07646be20874e516a6876da8f051
Esteban Esquivel <esquivelesteban0@gmail.com>
2022-11-03 16:02:51 -0500

    calculate the 50/30/20 rule
    using the income total calculate the 50/30/20 rule and display it at the end of the page

--- js/budget-planner.js
+++ js/budget-planner.js
@@ -18,14 +18,15 @@ incomeButton.onclick = calcIncome;
 expensesButton.onclick = expensesNeeds;
 // call the wants function
 wantsButton.onclick = wants;
-// call the savings function
 savingsButton.onclick = savings;
-
 // function to calculate the income
 function calcIncome() {
-    let wagesValue, otherValue, studentLoansValue, allowanceValue, incomeTotal;
+    let wagesValue, otherValue, studentLoansValue, allowanceValue, incomeTotal, expensesPercent, wantsPercent, savingsPercent;
     let incomeMessage = document.getElementById("income-message");
     let incomeEndTotals = document.getElementById("income-totals");
+    let expensesPercentTotal = document.getElementById("expenses-percent");
+    let wantsPercentTotal = document.getElementById("wants-percent");
+    let savingsPercentTotal = document.getElementById("savings-percent");
 
     wagesValue = parseFloat(wages.value);
     otherValue = parseFloat(other.value);
@@ -33,6 +34,9 @@ function calcIncome() {
     allowanceValue = parseFloat(allowance.value);
 
     incomeTotal = wagesValue + otherValue + studentLoansValue + allowanceValue;
+    expensesPercent = incomeTotal * 0.5;
+    wantsPercent = incomeTotal * 0.3;
+    savingsPercent = incomeTotal * 0.2;
 
     if (isNaN(wagesValue) || isNaN(otherValue) || isNaN(studentLoansValue) || isNaN(allowanceValue)) {
         incomeMessage.textContent = "Enter a valid input";
@@ -41,6 +45,9 @@ function calcIncome() {
     }
 
     incomeEndTotals.textContent = "Income: $" + incomeTotal;
+    expensesPercentTotal.textContent = "Save: $" + expensesPercent + " for your needs";
+    wantsPercentTotal.textContent = "Save: $" + wantsPercent + "  for your wants";
+    savingsPercentTotal.textContent = "Save: $" + savingsPercent + " for your savings";
 }
 
 // function to calculate the expenses
@@ -65,7 +72,7 @@ function expensesNeeds() {
     tuitionValue = parseFloat(tuition.value);
     booksValue = parseFloat(books.value);
     roomValue = parseFloat(room.value);
-    otherValue = parseFloat(other.value);
+    otherValue = parseFloat(otherE.value);
 
     expensesTotal = rentValue + carPaymentValue + autoInsuranceValue + carMaintenanceValue + gasolineValue + 
     groceriesValue + transportationValue + phoneBillValue + internetBillValue + studentLoanValue + tuitionValue +
@@ -79,7 +86,7 @@ function expensesNeeds() {
     } else {
         expensesMessage.textContent = "Total: $" + expensesTotal;
     }
-    expensesEndTotals.textContent = "Expenses: $" + expensesTotal;
+    expensesEndTotals.textContent = "Needs: $" + expensesTotal;
 }
 
 // function to calculate the wants
@@ -100,7 +107,7 @@ function wants() {
     streamingvalue = parseFloat(streaming.value);
     homeValue = parseFloat(home.value);
     sportsValue = parseFloat(sports.value);
-    otherValue = parseFloat(other.value);
+    otherValue = parseFloat(otherW.value);
 
     wantsTotal = clothingValue + diningValue + mealsValue + alcoholValue + movieValue + 
     gymValue + travelValue + streamingvalue + homeValue + sportsValue + otherValue;
@@ -113,4 +120,28 @@ function wants() {
         wantsMessage.textContent = "Total: $" + wantsTotal;
     }
     wantsEndTotals.textContent = "Wants: $" + wantsTotal;
+}
+
+function savings() {
+    let savingValue, emergencyValue, four1kValue, iraValue, investmentsValue, otherValue, savingsTotal;
+
+    let savingsMessage = document.getElementById("savings-message");
+    let savingsEndTotals = document.getElementById("savings-totals");
+
+    savingValue = parseFloat(saving.value);
+    emergencyValue = parseFloat(emergency.value);
+    four1kValue = parseFloat(four1k.value);
+    iraValue = parseFloat(ira.value);
+    investmentsValue = parseFloat(investments.value);
+    otherValue = parseFloat(otherS.value);
+
+    savingsTotal = savingValue + emergencyValue + four1kValue + iraValue + investmentsValue + otherValue;
+
+    if (isNaN(savingValue) || isNaN(emergencyValue) || isNaN(four1kValue) || isNaN(iraValue) || isNaN(investmentsValue)
+    || isNaN(otherValue)) {
+        savingsMessage.textContent = "Enter a valid input";
+    } else {
+        savingsMessage.textContent = "Total: $" + savingsTotal;
+    }
+    savingsEndTotals.textContent = "Savings: $" + savingsTotal;
 }
